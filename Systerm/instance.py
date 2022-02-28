d92358ac3113d48eb9fb5590a7714e9b16d2ce07
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 16:45:36 +0800

    Module class for Systerm.instance

--- Systerm/instance.py
+++ Systerm/instance.py
@@ -1,2 +1,8 @@
 """Instance is used for managing instances and classes"""
 import Systerm
+
+# InstanceMod
+@Systerm.module.add(__name__)
+class InstanceMod(Systerm.Module):
+	"""Module class for Systerm.instance"""
+	pass
