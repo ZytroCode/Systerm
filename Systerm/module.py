34badedf57265a29907e8444185533075d69b5f6
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 11:34:52 +0800

    Module class doesn't add double underscore (__) attributes

--- Systerm/module.py
+++ Systerm/module.py
@@ -4,7 +4,12 @@ import sys
 
 # Module class
 class Module(sys.modules[__name__].__class__):
-	"""Module class is used for creating a python module"""
+	"""Module class is used for creating a python module
+	If you don't want an attribute to be in the module, use __ before the name of the attribute"""
 	def __init__(self, name):
 		super().__init__(name)
-		sys.modules[name].__class__ = self.__class__
+		for attr in dir(self):
+			if len(attr) <= 2:
+				setattr(self, attr, getattr(self, attr))
+			elif attr[0] != "_" and attr[1] != "_":
+				setattr(self, attr, getattr(self, attr))
