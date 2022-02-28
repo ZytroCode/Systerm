fe5debcc93bfad2e5a5b8c2279f7d76b4c1a0b26
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 10:14:29 +0800

    Update __init__.py
    Import Systerm.module

--- Systerm/__init__.py
+++ Systerm/__init__.py
@@ -1,5 +1,9 @@
 """Systerm is a multipurpose python library"""
+
+# Adding Systerm to path
 import sys
 sys.path.append("..")
-
 del sys
+
+# Importing libraries in Systerm
+from Systerm import module
