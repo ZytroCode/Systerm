3e0442cea6f8a7998cf5d58092b6265e9451352e
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 17:00:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 # Metadata
 METADATA = dict(
-	name = "Systerm",
-	version = "0.1.0",
-	author = "ZytroCode",
-	packages = find_packages(),
+	name =			"Systerm",
+	version =		"0.1.0",
+	description =	"A multipurpose python library",
+	author =		"ZytroCode",
+	packages =		find_packages(),
 )
 
 setup(**METADATA)
