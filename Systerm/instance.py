7c09d972351842ccb0b34876e6425af26bc572f2
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 16:47:07 +0800

    Added super function
    Inherits an instance to a class

--- Systerm/instance.py
+++ Systerm/instance.py
@@ -5,4 +5,11 @@ import Systerm
 @Systerm.module.add(__name__)
 class InstanceMod(Systerm.Module):
 	"""Module class for Systerm.instance"""
-	pass
+	# Super function
+	def super(instance):
+		"""Inherits an instance to a class"""
+		def wrapper(cls):
+			for attr in dir(instance):
+				setattr(cls, attr, getattr(instance, attr))
+			return cls
+		return wrapper
