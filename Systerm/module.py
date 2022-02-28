94e346938a25f55f486397f50632e771f33f01d2
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 10:08:44 +0800

    Added Module class

--- Systerm/module.py
+++ Systerm/module.py
@@ -1,2 +1,10 @@
 """Module is used for optimizing python modules"""
 import Systerm
+import sys
+
+# Module class
+class Module(sys.modules[__name__].__class__):
+	"""Module class is used for creating a python module"""
+	def __init__(self, name):
+		super().__init__(name)
+		sys.modules[name].__class__ = self.__class__
