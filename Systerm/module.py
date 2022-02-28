8c23de567710c4e5cd96fb967031b07e8d44f20f
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 11:36:03 +0800

    Added add function
    Makes a module importable

--- Systerm/module.py
+++ Systerm/module.py
@@ -13,3 +13,25 @@ class Module(sys.modules[__name__].__class__):
 				setattr(self, attr, getattr(self, attr))
 			elif attr[0] != "_" and attr[1] != "_":
 				setattr(self, attr, getattr(self, attr))
+
+# Add function
+def add(*args):
+	"""Makes a module importable"""
+	error = Exception("add() unknown error")
+
+	if len(args) == 2:
+		try:
+			sys.modules[args[1]] = args[0](args[1])
+			return args[0](args[1])
+		except TypeError:
+			error = TypeError(f"add() takes 2 positional arguments but {len(args) + 1} were given")
+	elif len(args) == 1:
+		def wrapper(module):
+			sys.modules[args[0]] = module(args[0])
+			return module
+		return wrapper
+	elif len(args) == 0:
+		error = TypeError(f"add() missing 2 required positional arguments: 'module', 'test'")
+	elif len(args) >= 3:
+		error = TypeError(f"add() takes 2 positional arguments but {len(args)} were given")
+	raise error
