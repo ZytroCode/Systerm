aa88ba41c2d8f7b9f47d675bd072e0209afc9169
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-02-28 09:58:57 +0800

    Create setup.py
    Initialization for setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+# Metadata
+METADATA = dict(
+	name = "Systerm",
+	version = "0.1.0",
+	author = "ZytroCode",
+	packages = find_packages(),
+)
+
+setup(**METADATA)
