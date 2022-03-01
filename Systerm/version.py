bcc3eccb0d991ddbe71e72e164a8fe7176f5f78e
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-03-01 10:23:42 +0800

    Module class for Systerm.version.py

--- Systerm/version.py
+++ Systerm/version.py
@@ -1,3 +1,9 @@
 """Version contains the version of Systerm
 This can also be used to create a version of something of yours"""
 import Systerm
+
+# VersionMod
+@Systerm.module.add(__name__)
+class VersionMod(Systerm.Module):
+	"""Module class for Systerm.version"""
+	pass
