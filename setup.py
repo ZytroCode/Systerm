c43120ca6c4f66398db0c2ae3e8c1298f830a9b6
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-03-01 10:33:53 +0800

    Update setup.py
    Added LONG_DESCRIPTION variable
    Added CLASSIFIERS variable

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import setup, find_packages
 
+# Long description
+with open("README.md", encoding="UTF-8") as f:
+	LONG_DESCRIPTION = f.read()
+
+# Classifiers
+CLASSIFIERS = [
+	"Development Status :: 3 - Alpha",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.6",
+	"Programming Language :: Python :: 3.7",
+	"Programming Language :: Python :: 3.8",
+	"Programming Language :: Python :: 3.9",
+	"Programming Language :: Python :: 3.10",
+	"Programming Language :: Python :: 3.11",
+	"License :: OSI Approved :: MIT License"
+]
+
 # Metadata
 METADATA = dict(
 	name =			"Systerm",
