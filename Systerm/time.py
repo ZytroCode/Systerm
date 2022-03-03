b853e89f2f2df48103a4cf8248643f584bcb9eed
Zytro <omarwt92@gmail.com>
2022-03-03 10:25:48 +0800

    Added module class for Systerm.time

--- Systerm/time.py
+++ Systerm/time.py
@@ -1,2 +1,11 @@
 """This module is a copy of the python time module"""
 import Systerm
+import time
+
+# TimeMod
+@Systerm.module.add(__name__)
+@Systerm.instance.super(time)
+class TimeMod(Systerm.Module):
+	"""Module class for Systerm.time"""
+
+del time
