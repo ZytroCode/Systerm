59cef193d0de63daa44e84d76abc0b76d9c95550
Zytro <omarwt92@gmail.com>
2022-03-03 11:44:22 +0800

    Added module class for Systerm.console

--- Systerm/console.py
+++ Systerm/console.py
@@ -1,3 +1,6 @@
 """Console is used for interpreting the stream"""
 import Systerm
 import sys
+@Systerm.module.add(__name__)
+class ConsoleMod(Systerm.Module):
+	"""Module class for Systerm.console"""
