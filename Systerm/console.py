034a7d76a6f655682c65f1f255e0fe5f412e27d8
Zytro <omarwt92@gmail.com>
2022-03-03 11:44:48 +0800

    Added write function in Systerm.console

--- Systerm/console.py
+++ Systerm/console.py
@@ -1,6 +1,10 @@
 """Console is used for interpreting the stream"""
 import Systerm
 import sys
+
 @Systerm.module.add(__name__)
 class ConsoleMod(Systerm.Module):
 	"""Module class for Systerm.console"""
+	def write(self, value: object) -> None:
+		"""Writes a value to the stream"""
+		sys.stdout.write(str(value))
