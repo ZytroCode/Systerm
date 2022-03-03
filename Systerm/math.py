44dc852d2b662ad3ae57f42035229e5fd4efef69
Zytro <omarwt92@gmail.com>
2022-03-03 11:55:27 +0800

    Module class for Systerm.math

--- Systerm/math.py
+++ Systerm/math.py
@@ -1,2 +1,9 @@
 """Mathematical module for Systerm"""
 import Systerm
+import math
+
+# MathMod
+@Systerm.module.add(__name__)
+@Systerm.instance.super(math)
+class MathMod(Systerm.Module):
+	"""Module class for Systerm.math"""
