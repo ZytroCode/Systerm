77414b4153463c02efd21247b161fadca31ab610
Zytro <omarwt92@gmail.com>
2022-03-04 10:32:03 +0800

    Update exit.py
    Import atext

--- Systerm/exit.py
+++ Systerm/exit.py
@@ -1,8 +1,11 @@
 """Exit is a module from Systerm that manages everything on exit"""
 import Systerm
+import atexit
 
 # ExitMod
 @Systerm.module.add(__name__)
 @Systerm.instance.super(atexit)
 class ExitMod(Systerm.Module):
 	"""Module class for Systerm.Exit"""
+
+del atexit
