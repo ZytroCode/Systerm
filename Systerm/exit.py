3e76be55cb5faea438a06e5bd4f83e0ab96177b6
Zytro <omarwt92@gmail.com>
2022-03-04 10:31:33 +0800

    Module class for Systerm.exit

--- Systerm/exit.py
+++ Systerm/exit.py
@@ -1,2 +1,8 @@
 """Exit is a module from Systerm that manages everything on exit"""
 import Systerm
+
+# ExitMod
+@Systerm.module.add(__name__)
+@Systerm.instance.super(atexit)
+class ExitMod(Systerm.Module):
+	"""Module class for Systerm.Exit"""
