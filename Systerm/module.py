dacefc139cc29df4ddd82afeee66a58188671a5d
Zytro <omarwt92@gmail.com>
2022-03-04 11:25:28 +0800

    Update module.py
    Fix comment

--- Systerm/module.py
+++ Systerm/module.py
@@ -7,8 +7,7 @@ modules: dict = sys.modules
 
 # Module class
 class Module(sys.modules[__name__].__class__):
-	"""Module class is used for creating a python module
-	If you don't want an attribute to be in the module, use __ before the name of the attribute"""
+	"""Module class is used for creating a python module"""
 	_attrs: dict = {}
 	_hidden_prefixs: list = ["_"]
 	_hidden_items: list = []
