345d6bf814b5c863f3c52844a654963de6bc52bc
Zytro <omarwt92@gmail.com>
2022-03-08 11:21:47 +0800

    Added Logger class in Systerm.console

--- Systerm/console.py
+++ Systerm/console.py
@@ -32,13 +32,28 @@ class BaseLogger(object):
 	def listen(self, **keys):
 		"""Listens for an input in the stream"""
 		return input(self.format % keys)
+
+# Logger class
+class Logger(BaseLogger):
+	"""The main logging class for Systerm"""
+
+	def __init__(self, name: str, format: str=formats["Systerm"], **keys) -> None:
+		self.name = name
+		self.format = format
+		self.keys = keys
+		self.keys["name"] = name
+
+	def log(self, msg: str) -> None:
+		"""Logs a message to the stream"""
+		keys = deepcopy(self.keys)
+		keys["msg"] = msg
+		print(self.format % keys)
 	
-	def scan(self, prompt: Any) -> str:
-		"""More like input but simpler"""
-		self.send(prompt)
-		for line in sys.stdin:
-			return line.strip()
-		
+	def listen(self, msg: str) -> str:
+		"""Listens for an input in the stream"""
+		keys = deepcopy(self.keys)
+		keys["msg"] = msg
+		return input(self.format % keys)
 	def call(self, command: str) -> int:
 		"""Execute a command in a subshell"""
 		return os.system(command)
