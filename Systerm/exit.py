56cccc0f8dad660cc829d4e70dbfc303250aeb02
Zytro <omarwt92@gmail.com>
2022-03-08 11:24:42 +0800

    Tweaks in Systerm.exit

--- Systerm/exit.py
+++ Systerm/exit.py
@@ -6,15 +6,15 @@ import atexit
 # ExitMod
 class ExitMod(Systerm.module.Module.super(atexit).__class__):
 	"""Module class for Systerm.Exit"""
-	def exit(self, msg=None):
-		sys.exit(msg)
-	
-	@atexit.register
-	def _at_exit() -> None:
-		if Systerm.get_installed():
-			Systerm.uninstall()
-	
 	def __call__(self, msg=None):
 		self.exit(msg)
 
-del atexit
+	def exit(self, msg=None):
+		sys.exit(msg)
+	
+	@register
+	def _() -> None:
+		if Systerm.get_installed():
+			Systerm.uninstall()
+
+Systerm.module.modules[__name__].__class__ = ExitMod
