34916f3fd04b965fa37f7adaaaaccf2eaa4962a4
Zytro <omarwt92@gmail.com>
2022-03-08 15:18:57 +0800

    Added module class for Systerm.random

--- Systerm/random.py
+++ Systerm/random.py
@@ -1,2 +1,7 @@
 """This module is a copy of the python's random module"""
 import Systerm
+import random
+
+# RandomMod
+class RandomMod(Systerm.module.Module.super(random)):
+	"""Module class for Systerm.random"""
