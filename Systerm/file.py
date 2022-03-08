48b7f27ab931a9aa6866dbe1e8af0d8c9ed9d5d8
Zytro <omarwt92@gmail.com>
2022-03-08 15:22:00 +0800

    Added module class for Systerm.file

--- Systerm/file.py
+++ Systerm/file.py
@@ -3,3 +3,8 @@ import Systerm
 import pathlib
 import shutil
 import os
+# FileMod
+class FileMod(Systerm.module.Module.super(shutil, os.path, pathlib)):
+	"""Module class for Systerm.file"""
+
+Systerm.module.modules[__name__].__class__ = FileMod
