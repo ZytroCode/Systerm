ed5cbdaf22ef89249cd8b07f79057c4c1c4ecd18
Zytro <omarwt92@gmail.com>
2022-03-08 11:09:22 +0800

    Added Metaclass

--- Systerm/meta.py
+++ Systerm/meta.py
@@ -1,2 +1,6 @@
 """Meta is a module contains objects that will customize the behavior of python"""
 import Systerm
+
+# Metaclass
+class Metaclass(ABCMeta):
+	"""A metaclass to customize the behavior of all classes"""
