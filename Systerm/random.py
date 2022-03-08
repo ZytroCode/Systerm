f9d7974f8f28b8dca01f917c44f1da0aa191a2e2
Zytro <omarwt92@gmail.com>
2022-03-08 15:19:34 +0800

    Added random methods for Systerm.random

--- Systerm/random.py
+++ Systerm/random.py
@@ -5,6 +5,13 @@ import random
 # RandomMod
 class RandomMod(Systerm.module.Module.super(random)):
 	"""Module class for Systerm.random"""
+	int = random.randint
+	float = random.uniform
+	bool = lambda self:self.random() < 0.5
+	range = random.randrange
+	__call__ = lambda self:self.random()
+	randfloat = float
+	randbool = bool
 
 del random
 Systerm.module.modules[__name__].__class__ = RandomMod
