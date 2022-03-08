3f26d881f51d55313caf565ea306c05d0e854712
Zytro <omarwt92@gmail.com>
2022-03-08 15:22:26 +0800

    Added BaseFile class in Systerm.file

--- Systerm/file.py
+++ Systerm/file.py
@@ -3,6 +3,33 @@ import Systerm
 import pathlib
 import shutil
 import os
+
+from typing import Optional, Union, NewType
+
+# BaseFile class
+class BaseFile(metaclass=Systerm.meta.Metaclass):
+	"""Used for interpreting a file and it's contents"""
+	def __init__(self, path: str, mode: Optional[Union[str, type]]="file") -> None:
+		def set_mode(mode):
+			for name in dir(mode):
+				if not name.startswith("_"):
+					setattr(self, name, getattr(mode(path), name))
+			self.__class__ = mode
+		
+		if type(mode) != str and issubclass(mode, BaseFile):
+			set_mode(mode)
+		
+		elif mode == "file":
+			set_mode(File)
+	
+	def __enter__(self):
+		return self
+	
+	def __exit__(self, type, value, traceback):
+		self.close()
+	
+	def close(self):
+		del self
 # FileMod
 class FileMod(Systerm.module.Module.super(shutil, os.path, pathlib)):
 	"""Module class for Systerm.file"""
