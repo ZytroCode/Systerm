fb89b755c3335a250c6133fadab5f3591309cb8a
Zytro <omarwt92@gmail.com>
2022-03-08 11:09:56 +0800

    Added __new__ in Metaclass

--- Systerm/meta.py
+++ Systerm/meta.py
@@ -4,3 +4,9 @@ import Systerm
 # Metaclass
 class Metaclass(ABCMeta):
 	"""A metaclass to customize the behavior of all classes"""
+
+	def __new__(self, name: str, bases: tuple, attrs: dict, **keys) -> type:
+		# Creating a new class
+		cls = super().__new__(self, name, bases, dict(attrs))
+		
+		return cls
