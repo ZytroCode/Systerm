e57ae7d7011e49ff08d883bf76851915741ce80c
Zytro <omarwt92@gmail.com>
2022-03-11 10:39:30 +0800

    Tabs to spaces in Systerm/time.py

--- Systerm/time.py
+++ Systerm/time.py
@@ -6,7 +6,7 @@ import Systerm
 
 # TimeMod class
 class TimeMod(Systerm.module.Module.super(date, time)):
-	__call__ = get = lambda self:self.time()
+    __call__ = get = lambda self:self.time()
 
 del time
 Systerm.module.modules[__name__].__class__ = TimeMod
