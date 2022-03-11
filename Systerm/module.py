8a2c4d258b700a8bfbba67699bba8e4079fc3809
Zytro <omarwt92@gmail.com>
2022-03-11 10:39:16 +0800

    Tabs to spaces in Systerm/module.py

--- Systerm/module.py
+++ Systerm/module.py
@@ -13,38 +13,38 @@ modules: dict = sys.modules
 
 # Module class
 class Module(sys.modules[__name__].__class__, metaclass=meta.Metaclass):
-	"""Creates a new python module."""
-	def __init__(self, name: str) -> None:
-		"""The constructor for the Module class.
+    """Creates a new python module."""
+    def __init__(self, name: str) -> None:
+        """The constructor for the Module class.
 
-		Parameters:
-			name - str:	The name of the module
-		"""
-		super().__init__(name)
-		for attr in dir(self):
-			if name in self.__namespaces__:
-				setattr(self, attr, getattr(self, attr))
+        Parameters:
+            name - str:	The name of the module
+        """
+        super().__init__(name)
+        for attr in dir(self):
+            if name in self.__namespaces__:
+                setattr(self, attr, getattr(self, attr))
 
-	def __dir__(self) -> list:
-		return self.__namespaces__
+    def __dir__(self) -> list:
+        return self.__namespaces__
 
-	@staticmethod
-	def super(*instances: Any):
-		"""Creates a new class that inherits instances.
+    @staticmethod
+    def super(*instances: Any):
+        """Creates a new class that inherits instances.
 
-		Parameters:
-			*instances - Any:	The instances to be inherit
-		"""
-		cls = meta.Metaclass("_", (Module,), dict())
+        Parameters:
+            *instances - Any:	The instances to be inherit
+        """
+        cls = meta.Metaclass("_", (Module,), dict())
 
-		for instance in instances:
-			for name in dir(instance):
-				setattr(cls, name, getattr(instance, name))
+        for instance in instances:
+            for name in dir(instance):
+                setattr(cls, name, getattr(instance, name))
 
-		return cls
+        return cls
 
 # ModuleMod class
 class ModuleMod(Module):
-	pass
+    pass
 
 modules[__name__].__class__ = ModuleMod
