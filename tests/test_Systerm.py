11f2b898b525b680e544fa083b1efde84ebc2a48
Zytro <omarwt92@gmail.com>
2022-03-12 09:55:43 +0800

    Update test_Systerm.py

--- tests/test_Systerm.py
+++ tests/test_Systerm.py
@@ -1,7 +1,3 @@
 def test_main():
     import Systerm
     Systerm.install()
-
-    assert Systerm.version
-    assert Systerm.Metaclass
-    assert Systerm.get_installed(), "Systerm was not installed correctly"
